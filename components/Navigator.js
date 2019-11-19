c08f835c1a03e22dda847ac2a3af1b8575327a44
yasora <yasora@gmail.com>
2019-11-19 11:49:55 +0530

    tabs added

--- components/Navigator.js
+++ components/Navigator.js
@@ -4,16 +4,41 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './HomeScreen';
 import DetailsScreen from './DetailsScreen';
 import ToDoApp from "./TodoApp";
+import {createMaterialTopTabNavigator} from "react-navigation-tabs";
 
+const TabScreen = createMaterialTopTabNavigator(
+    {
+        Home: { screen: ToDoApp },
+        Settings: { screen: DetailsScreen },
+    },
+    {
+        tabBarPosition: 'top',
+        swipeEnabled: true,
+        animationEnabled: true,
+        tabBarOptions: {
+            activeTintColor: '#FFFFFF',
+            inactiveTintColor: '#F8F8F8',
+            style: {
+                backgroundColor: '#486d89',
+            },
+            labelStyle: {
+                textAlign: 'center',
+            },
+            indicatorStyle: {
+                borderBottomColor: '#87B56A',
+                borderBottomWidth: 2,
+            },
+        },
+    }
+);
 const Navigator = createStackNavigator(
     {
         Home: HomeScreen,
         Details: DetailsScreen,
-        Todo: ToDoApp
+        Todo: TabScreen
     },
     {
         initialRouteName: 'Home',
     }
 );
-
 export default createAppContainer(Navigator);
